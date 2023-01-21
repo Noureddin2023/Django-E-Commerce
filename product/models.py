132d3cd3c3d4276823a682f2ac38194bef80aeb9
Noureddin2023 <nam2021sa@gmail.com>
2023-01-21 20:10:10 +0100

    add class

--- product/models.py
+++ product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    pass
+
+
+
+
+
+class images(models.Model):
+    pass
+
+
+
+
+class brand(models.Model):
+    pass
+
+
+
+class reviews(models.Model):
+    pass        
