08ca760eadec284aa3be88aa03e36d0a2d71f53a
Noureddin2023 <nam2021sa@gmail.com>
2023-01-22 14:43:25 +0100

    design product models

--- product/models.py
+++ product/models.py
@@ -1,23 +1,29 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 PRODUCT_FLAG = (
-    ('Sale , Sales'),
-    ('Feature' , 'Feature'),
-    ('New' , 'New'),
+    ('Sale','Sales'),
+    ('Feature','Feature'),
+    ('New','New'),
 
 class Product(models.Model):
     
-    name = models.CharField(max_length=150)
-    image = models.ImageField(upload_to='posts/', default='default.png')
-    flag = models.CharField(max_length=10,choices=PTODUCT_FLAG)
-    price = models.FloatField()
-    sku = models.IntegerField()
-    brand = ''
-    tags = ''
-    subtitle = models.TextField(max_length=500)
-    description = models.TextField(max_length=2000)
+    name = models.CharField(_('name'),max_length=150)
+    image = models.ImageField(_('image'),upload_to='posts/', default='default.png')
+    flag = models.CharField(_('flag')max_length=10,choices=PTODUCT_FLAG)
+    price = models.FloatField(_('price'))
+    sku = models.IntegerField(_('sku'))
+    brand = models.ForeignKey('Brand', verbose_name=_('brand'), related_name='product_brand', on_delete=models.CASCADE)
+    tags = TaggableManager()
+    subtitle = models.TextField(_('subtitle'),max_length=500)
+    description = models.TextField(_('description'),max_length=2000)
+
+
+    def__str__(self):
+        return self.name
     '''
     name
     image
