9f886c21d343f0eb36559fac5b7f4783aaa67010
Noureddin2023 <nam2021sa@gmail.com>
2023-01-22 13:49:49 +0100

    add models

--- product/models.py
+++ product/models.py
@@ -2,8 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+PRODUCT_FLAG = (
+    ('Sale , Sales'),
+    ('Feature' , 'Feature'),
+    ('New' , 'New'),
+
 class Product(models.Model):
-    pass
+    
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='posts/', default='default.png')
+    flag = models.CharField(max_length=10,choices=PTODUCT_FLAG)
+    price = models.FloatField()
+    sku = models.IntegerField()
+    brand = ''
+    tags = ''
+    subtitle = models.TextField(max_length=500)
+    description = models.TextField(max_length=2000)
     '''
     name
     image
