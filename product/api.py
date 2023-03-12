09293ee8e431c22597a6de28bd012fd2a574862f
Noureddin2023 <nam2021sa@gmail.com>
2023-03-12 19:31:28 +0100

    show product images in detail api

--- product/api.py
+++ product/api.py
@@ -2,23 +2,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
 from .models import Product , Brand
-from .serializers import ProductSerializer , BrandDetailSerializer , BrandListSerializer
+from .serializers import ProductListSerializer , ProductDetailSerializer , BrandDetailSerializer , BrandListSerializer
 
 @api_view(['GET'])
 def productlist_api(request):
     products= Product.objects.all()
-    data = ProductSerializer(products,many=True,context={"request":request}).data
+    data = ProductListSerializer(products,many=True,context={"request":request}).data
     return Response({'data':data})
 
 
 
 class ProductListApi(generics.ListCreateAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
    
 class ProductDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer  
+    serializer_class = ProductDetailSerializer 
     lookup_field='slug'  
 
 
