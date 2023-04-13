d9805d284064b2e11b5dd5d152c479f586da3a77
Noureddin2023 <nam2021sa@gmail.com>
2023-04-13 17:31:47 +0200

    create accounts App , design user models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile , ContactNumbers , Address
+
+admin.site.register(Profile)
+admin.site.register(ContactNumbers)
+admin.site.register(Address)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from utils.generate_code import generate_code
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile/')
+
+    code = models.CharField(max_length=8 , default=generate_code)
+
+Number_Choices = (
+    ('Primary', 'Primary'),
+    ('Secondery', 'Secondery')
+)
+
+
+class ContactNumbers(models.Model):
+    user = models.ForeignKey(User,related_query_name='user_contacts' ,on_delete=models.CASCADE) 
+    type = models.CharField(max_length=10 , choices=Number_Choices)   
+    number = models.CharField(max_length=20)
+
+Address_Choices = (
+   
+    ('Home','Home'),
+    ('Office','Office'),
+    ('Bussiness','Bussiness'),
+    ('Academy','Academy'),
+    ('Others','Othres')
+)
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User,related_query_name='user_address' ,on_delete=models.CASCADE) 
+    type = models.CharField(max_length=10 , choices=Address_Choices)
+    city = models.CharField( max_length=20)
+    street = models.CharField( max_length=20)
+    house = models.CharField( max_length=20)
+    notes = models.CharField( max_length=200)
